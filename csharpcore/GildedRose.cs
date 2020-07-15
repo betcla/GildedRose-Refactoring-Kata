da8f97e68eea669b7f7252e0531db340772d4b9d
BetCla <bethany.clarke@softwire.com>
2020-07-15 14:23:46 +0100

    Refactoring functionality of other objects (test passes)

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -21,7 +21,7 @@ namespace csharpcore
 
                     if (Items[i].Name == "Aged Brie")
                     {
-                        if ((Items[i].SellIn > 0) && (Items[i].Quality < 50))
+                        if ((Items[i].SellIn >= 0) && (Items[i].Quality < 50))
                         {
                             Items[i].Quality = Items[i].Quality + 1;
                         }
@@ -33,7 +33,7 @@ namespace csharpcore
                     
                     else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if ((Items[i].Quality < 50)&&(Items[i].SellIn > 0))
+                        if ((Items[i].Quality < 50)&&(Items[i].SellIn >= 0))
                         {
                             Items[i].Quality = Items[i].Quality + 1;
                             if ((Items[i].Quality < 50) && (Items[i].SellIn < 10))
@@ -45,10 +45,28 @@ namespace csharpcore
                                 }
                             }
                         }
-                        else if (Items[i].SellIn <= 0)
+                        else if (Items[i].SellIn < 0)
                         {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Items[i].Quality = 0;
                         }
+                    }
+
+                    else
+                    {
+                        if (Items[i].Quality > 0) 
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                            if ((Items[i].SellIn < 0)&&(Items[i].Quality>0))
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                        }
+
+                        if (Items[i].Quality == 0)
+                        {
+                            Items[i].Quality = 0;
+                        }
+
                     }
                     
 
